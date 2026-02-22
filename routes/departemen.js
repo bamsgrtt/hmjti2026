8938595b9e7c50cff2f3dc08750c1cd6fb09f913
bamsgrtt <genjeh1927@gmail.com>
2026-02-22 20:13:13 +0700

    change query on routes

--- routes/departemen.js
+++ routes/departemen.js
@@ -6,16 +6,29 @@ const db = require('../db');
 router.get("/", (req, res) => {
     // Query 1: Ambil semua data anggota untuk kartu
     const anggotaQuery = `SELECT * FROM anggota WHERE id >= 2 ORDER BY id ASC`;
-    
+
     // Query 2: Ambil daftar divisi unik untuk tombol filter
-    const divisiQuery = `SELECT DISTINCT divisi FROM anggota ORDER BY divisi ASC`;
+    const divisiQuery = `
+        SELECT DISTINCT divisi
+        FROM anggota
+        ORDER BY
+        CASE divisi
+        WHEN 'BPH' THEN 1
+        WHEN 'Administrasi' THEN 2
+        WHEN 'Keilmuan' THEN 3
+        WHEN 'Perhubungan' THEN 4
+        WHEN 'Kominfo' THEN 5
+        WHEN 'Kewirausahaan' THEN 6
+        ELSE 99
+        END
+        `;
 
     db.query(anggotaQuery, (err, anggotaResult) => {
         if (err) return res.status(500).send("Database Error (Anggota)");
 
         db.query(divisiQuery, (err, divisiResult) => {
             if (err) return res.status(500).send("Database Error (Divisi)");
-            
+
             res.render('departemen', {
                 title: 'DEPARTEMEN',
                 anggota: anggotaResult,
